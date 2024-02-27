a095f8f3e2418813915025ee652155170d8b6449
JoaquimARM <joaquimarm13@gmail.com>
2024-02-27 11:48:06 -0300

    calculadora imc

--- IMC_com_interface/js/script.js
+++ IMC_com_interface/js/script.js
@@ -10,12 +10,17 @@ formulario.addEventListener('submit',function(e){//a funçao aguarda o usuario c
     const inputaltura= e.target.querySelector('#altura');
     const altura=parseFloat(inputaltura.value);
 
-    console.log(inputpeso);
-    console.log(peso);
+    const imc = calcularImc(peso,altura)
 
-    console.log(inputaltura)
+    console.log(peso)
     console.log(altura)
-
-
-
+    console.log(imc)
 })
+
+//função para calcular o imc e retornar o seu valor
+
+function calcularImc(peso,altura){
+    let imc = (peso/(altura**2)); //formula do imc
+    console.log(imc)
+    return imc.toFixed(2); //tofixed para formatar casa decimal
+}
